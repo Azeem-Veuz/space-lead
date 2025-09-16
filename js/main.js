e306fc5f418c9e2292f49a53be3bbb41377d1527
Azeem-Veuz <azeemulyazin@veuz.in>
2025-09-16 09:56:36 +0530

    new design changes

--- js/main.js
+++ js/main.js
@@ -425,6 +425,34 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+$(".news-carousel").owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: true,
+  autoplay: true,
+  dots: false,
+  navText: [
+    '<i class="fa-solid fa-arrow-left"></i>',
+    '<i class="fa-solid fa-arrow-right"></i>'
+  ],
+  responsive: {
+    0: {
+      items: 1
+    },
+    768: {
+      items: 2
+    },
+    992: {
+      items: 3
+    },
+    1200: {
+      items: 4
+    }
+  }
+});
+
+
+
 
 
 
